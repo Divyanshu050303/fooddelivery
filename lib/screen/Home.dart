ac21e1c3b9975c0fded126202202cefa4f075a8c
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2023-06-06 08:52:46 +0530

    Update the item-list and add quantity

--- lib/screen/Home.dart
+++ lib/screen/Home.dart
@@ -39,11 +39,11 @@ class _HomeScreenState extends State<HomeScreen> {
                 SizedBox(
                   width: mediaQueryData.size.width * 0.059,
                 ),
-                 const ItemCard(name: "Masala Dosa",image: "assets/masalaDosa.png",price:"60"),
+                 const ItemCard(name: "Masala Dosa",image: "assets/masalaDosa.png",price:"60", quantity: '1',),
                 SizedBox(
                   width: mediaQueryData.size.width * 0.06,
                 ),
-                const ItemCard(name: "Butter Dosa",image: "assets/butterdosa.png",price:"100"),
+                const ItemCard(name: "Butter Dosa",image: "assets/butterdosa.png",price:"100", quantity: "1",),
               ],
             ),
             SizedBox(
@@ -54,11 +54,11 @@ class _HomeScreenState extends State<HomeScreen> {
                 SizedBox(
                   width: mediaQueryData.size.width * 0.059,
                 ),
-                const ItemCard(name: "Idli Fry",image: "assets/idlifry.png",price:"70"),
+                const ItemCard(name: "Idli Fry",image: "assets/idlifry.png",price:"70", quantity: "1",),
                 SizedBox(
                   width: mediaQueryData.size.width * 0.06,
                 ),
-                const ItemCard(name: "Idli Sambar",image: "assets/idli.png",price:"60"),
+                const ItemCard(name: "Idli Sambar",image: "assets/idli.png",price:"60", quantity: "1",),
               ],
             ),
             const Padding(
@@ -73,11 +73,11 @@ class _HomeScreenState extends State<HomeScreen> {
                 SizedBox(
                   width: mediaQueryData.size.width * 0.059,
                 ),
-                const ItemCard(name: "Sprite ",image: "assets/Sptite.png",price:"45"),
+                const ItemCard(name: "Sprite ",image: "assets/Sptite.png",price:"45", quantity: "1",),
                 SizedBox(
                   width: mediaQueryData.size.width * 0.06,
                 ),
-                const ItemCard(name: "Thums up",image: "assets/thumsup.png",price:"45"),
+                const ItemCard(name: "Thums up",image: "assets/thumsup.png",price:"45", quantity: "1",),
               ],
             ),
             SizedBox(
@@ -88,11 +88,11 @@ class _HomeScreenState extends State<HomeScreen> {
                 SizedBox(
                   width: mediaQueryData.size.width * 0.059,
                 ),
-                const ItemCard(name: "Coke",image: "assets/coke.png",price:"45"),
+                const ItemCard(name: "Coke",image: "assets/coke.png",price:"45", quantity: "1",),
                 SizedBox(
                   width: mediaQueryData.size.width * 0.06,
                 ),
-                const ItemCard(name: "limca",image: "assets/limca.png",price:"95"),
+                const ItemCard(name: "limca",image: "assets/limca.png",price:"95", quantity: "1",),
               ],
             ),
             const Padding(
@@ -107,11 +107,11 @@ class _HomeScreenState extends State<HomeScreen> {
                 SizedBox(
                   width: mediaQueryData.size.width * 0.059,
                 ),
-                const ItemCard(name: "ButterScotch",image: "assets/buttersc.png",price:"400"),
+                const ItemCard(name: "ButterScotch",image: "assets/buttersc.png",price:"400", quantity: "1",),
                 SizedBox(
                   width: mediaQueryData.size.width * 0.06,
                 ),
-                const ItemCard(name: "Pineapple",image: "assets/pineapple.png",price:"400"),
+                const ItemCard(name: "Pineapple",image: "assets/pineapple.png",price:"400", quantity: "1",),
               ],
             ),
             SizedBox(
@@ -122,11 +122,11 @@ class _HomeScreenState extends State<HomeScreen> {
                 SizedBox(
                   width: mediaQueryData.size.width * 0.059,
                 ),
-                const ItemCard(name: "Strawberry",image: "assets/strawberry.png",price:"700"),
+                const ItemCard(name: "Strawberry",image: "assets/strawberry.png",price:"700", quantity: "1",),
                 SizedBox(
                   width: mediaQueryData.size.width * 0.06,
                 ),
-                const ItemCard(name: "Pihu ",image: "assets/pihu.png",price:"700"),
+                const ItemCard(name: "Pihu ",image: "assets/pihu.png",price:"700", quantity: "1",),
               ],
             ),
             Padding(
@@ -158,11 +158,11 @@ class _HomeScreenState extends State<HomeScreen> {
                     SizedBox(
                       width: mediaQueryData.size.width * 0.059,
                     ),
-                    const ItemCard(name: "Fry Paneer Momo's",image: "assets/frymomo.png",price:"80"),
+                    const ItemCard(name: "Fry Paneer Momo's",image: "assets/frymomo.png",price:"80", quantity: "1",),
                     SizedBox(
                       width: mediaQueryData.size.width * 0.06,
                     ),
-                    const ItemCard(name: "Momo's",image: "assets/momo.png",price:"50"),
+                    const ItemCard(name: "Momo's",image: "assets/momo.png",price:"50", quantity: "1",),
                   ],
                 ),
                 SizedBox(
@@ -173,11 +173,11 @@ class _HomeScreenState extends State<HomeScreen> {
                     SizedBox(
                       width: mediaQueryData.size.width * 0.059,
                     ),
-                    const ItemCard(name: "Paneer momo's steam",image: "assets/pneermomos.png",price:"70"),
+                    const ItemCard(name: "Paneer momo's steam",image: "assets/pneermomos.png",price:"70", quantity: "1",),
                     SizedBox(
                       width: mediaQueryData.size.width * 0.06,
                     ),
-          const ItemCard(name: "Fry momos",image: "assets/frymomo.png",price:"50"),
+          const ItemCard(name: "Fry momos",image: "assets/frymomo.png",price:"50", quantity: "1",),
                   ],
                 ),
           ])),
