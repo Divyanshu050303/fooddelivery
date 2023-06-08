603a50a8627f2f136751261a2e2e031a9c8d93c3
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2023-06-08 10:31:24 +0530

    try to make the item card adoptable according to  the screen.d

--- lib/screen/AddCart.dart
+++ lib/screen/AddCart.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../util/ItemCard.dart';
@@ -11,6 +10,7 @@ class AddCart extends StatefulWidget {
 }
 
 class _AddCartState extends State<AddCart> {
+
   @override
   Widget build(BuildContext context) {
     MediaQueryData mediaQueryData = MediaQuery.of(context);
@@ -23,19 +23,19 @@ class _AddCartState extends State<AddCart> {
               end: Alignment.bottomRight)),
       child: GridView.builder(
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
+            crossAxisCount: mediaQueryData.size.width>900?3:2,
             mainAxisSpacing: mediaQueryData.size.width * 0.09,
+            childAspectRatio: 0.8
           ),
           itemCount: ItemList.length,
           itemBuilder: (context, index) {
+            int qua=int.parse(ItemList[index]['quantity']);
             return Padding(
                 padding: EdgeInsets.only(
                     left: mediaQueryData.size.width * 0.045,
                     right: mediaQueryData.size.width * 0.045),
                 child: GridTile(
                     child: Container(
-                  width: 150,
-                  height: 300,
                   decoration: BoxDecoration(
                     gradient: LinearGradient(
                       colors: [
@@ -58,69 +58,49 @@ class _AddCartState extends State<AddCart> {
                           offset: const Offset(0, 3))
                     ],
                   ),
-                  child: Column(
-                    children: [
-                      Image.asset(
-                        ItemList[index]['image'],
-                        width: 150,
-                        height: 100,
-                        fit: BoxFit.fitWidth,
-                      ),
-                      Text(
-                        ItemList[index]['name'],
-                        style: const TextStyle(
-                            fontSize: 20,
-                            color: Colors.black,
-                            decoration: TextDecoration.none),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      Text(
-                        ItemList[index]['price'],
-                        style: const TextStyle(
-                            fontSize: 20,
-                            color: Colors.black,
-                            decoration: TextDecoration.none),
-                      ),
-                      IconButton(onPressed: (){}, icon: const Icon(Icons.delete)),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Container(
-                            width: 40,
-                            height: 20,
-                            decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                colors: [
-                                  Colors.pinkAccent.shade100,
-                                  Colors.white,
-                                  Colors.white
-                                ],
-                                begin: Alignment.topLeft,
-                                end: Alignment.bottomRight,
-                              ),
-                              border: Border.all(
-                                  style: BorderStyle.solid,
-                                  color: Colors.black,
-                                  width: 1),
-                              boxShadow: [
-                                BoxShadow(
-                                    color: Colors.black.withOpacity(0.5),
-                                    spreadRadius: 2,
-                                    blurRadius: 3,
-                                    offset: const Offset(0, 3))
-                              ],
-                            ),
-                            alignment: Alignment.center,
-                            child:const Text("-", style: TextStyle(fontSize: 20),),
-
-                          ),
-                          Container(
+                  child: ListView(
+                    children:[ Column(
+                      children: [
+                        Image.asset(
+                          ItemList[index]['image'],
+                          width: 150,
+                          height: 100,
+                          fit: BoxFit.fitWidth,
+                        ),
+                        Text(
+                          ItemList[index]['name'],
+                          style: const TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                              decoration: TextDecoration.none),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(
+                          ItemList[index]['price'],
+                          style: const TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                              decoration: TextDecoration.none),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            GestureDetector(
+                                onTap: (){
+                                  qua-=1;
+                                  setState(() {
+                                    ItemList[index]['quantity']=qua.toString();
+                                    if(qua.toString()=="0"){
+                                      ItemList.removeAt(index);
+                                    }
+                                  });
+                                },
+                            child:Container(
                               width: 40,
                               height: 20,
                               decoration: BoxDecoration(
-                                gradient: LinearGradient(
+                                gradient: const LinearGradient(
                                   colors: [
-                                    Colors.pinkAccent.shade100,
                                     Colors.white,
                                     Colors.white
                                   ],
@@ -140,46 +120,77 @@ class _AddCartState extends State<AddCart> {
                                 ],
                               ),
                               alignment: Alignment.center,
-                              child:  Text(ItemList[index]['quantity'])),
-                          GestureDetector(
-                            onTap: (){
-                              ItemList[index]['quantity']=(int.parse(ItemList[index]['quantity'])+1).toString();
-                              print(ItemList[index]['quantity']);
-                            },
-                            child:
-
-                          Container(
-                              width: 40,
-                              height: 20,
-                              decoration: BoxDecoration(
-                                gradient: LinearGradient(
-                                  colors: [
-                                    Colors.pinkAccent.shade100,
-                                    Colors.white,
-                                    Colors.white
+                              child:const Text("-" ),
+                            )),
+                            Container(
+                                width: 40,
+                                height: 20,
+                                decoration: BoxDecoration(
+                                  gradient: const LinearGradient(
+                                    colors: [
+                                      Colors.white,
+                                      Colors.white
+                                    ],
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight,
+                                  ),
+                                  border: Border.all(
+                                      style: BorderStyle.solid,
+                                      color: Colors.black,
+                                      width: 1),
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: Colors.black.withOpacity(0.5),
+                                        spreadRadius: 2,
+                                        blurRadius: 3,
+                                        offset: const Offset(0, 3))
                                   ],
-                                  begin: Alignment.topLeft,
-                                  end: Alignment.bottomRight,
                                 ),
-                                border: Border.all(
-                                    style: BorderStyle.solid,
-                                    color: Colors.black,
-                                    width: 1),
-                                boxShadow: [
-                                  BoxShadow(
-                                      color: Colors.black.withOpacity(0.5),
-                                      spreadRadius: 2,
-                                      blurRadius: 3,
-                                      offset: const Offset(0, 3))
-                                ],
-                              ),
-                              alignment: Alignment.center,child: const Text("+"),)),
-                        ],
-
-                      ),
-
-                    ],
-                  ),
+                                alignment: Alignment.center,
+                                child:  Text(ItemList[index]['quantity'])),
+                            GestureDetector(
+                              onTap: (){
+                                qua+=1;
+                                setState(() {
+                                  ItemList[index]['quantity']=qua.toString();
+                                });
+                              },
+                              child:
+                            Container(
+                                width: 40,
+                                height: 20,
+                                decoration: BoxDecoration(
+                                  gradient: const LinearGradient(
+                                    colors: [
+                                     Colors.white,
+                                      Colors.white
+                                    ],
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight,
+                                  ),
+                                  border: Border.all(
+                                      style: BorderStyle.solid,
+                                      color: Colors.black,
+                                      width: 1),
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: Colors.black.withOpacity(0.5),
+                                        spreadRadius: 2,
+                                        blurRadius: 3,
+                                        offset: const Offset(0, 3))
+                                  ],
+                                ),
+                                alignment: Alignment.center,child: const Text("+"),)),
+                          ],
+                        ),
+                        IconButton(onPressed: (){
+                          setState(() {
+                            ItemList.removeAt(index);
+                          });
+                        }, icon: const Icon(Icons.delete_forever_rounded ,color: Colors.black,))
+                      ],
+                    ),
+                  ]),
                 )));
           }),
     );
