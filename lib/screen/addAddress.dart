86200afe67a6e43030dab85c7653810c1d094445
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2023-06-08 10:30:25 +0530

    you can add the address and delete the address at this screen.

---
+++ lib/screen/addAddress.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Address extends StatefulWidget {
+  const Address({Key? key}) : super(key: key);
+
+  @override
+  State<Address> createState() => _AddressState();
+}
+
+class _AddressState extends State<Address> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("AddAddress");
+  }
+}
