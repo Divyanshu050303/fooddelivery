d35958b1e8ecdacd75c135aad12a778b6b4875d2
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2023-06-12 23:36:38 +0530

    Splash Screen

---
+++ lib/screen/SplashScreen.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:fooddelivery/NavBar.dart';
+import 'package:fooddelivery/screen/SignIn.dart';
+import 'package:video_player/video_player.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  late VideoPlayerController _videoPlayerController;
+  bool _loading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _videoPlayerController = VideoPlayerController.asset('assets/splash.mp4');
+    _videoPlayerController.initialize().then((_) {
+      setState(() {
+        _videoPlayerController.play();
+        _videoPlayerController.setLooping(true);
+      });
+    });
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const NavBar()));
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _videoPlayerController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          _videoPlayerController.value.isInitialized
+              ? AspectRatio(
+                  aspectRatio: _videoPlayerController.value.aspectRatio,
+                  child: VideoPlayer(_videoPlayerController),
+                )
+              : Container()
+        ],
+      ),
+    );
+  }
+}
