cf9592ecdee003bf26a83bc3a0b6d2486cac06d4
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-06-22 11:44:54 +0530

    Owner.dart

--- lib/screen/Owner.dart
+++ lib/screen/Owner.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fooddelivery/screen/AddItem.dart';
+import 'package:fooddelivery/screen/EditItem.dart';
 
 class Owner extends StatefulWidget {
   const Owner({Key? key}) : super(key: key);
@@ -17,53 +18,58 @@ class _OwnerState extends State<Owner> {
       height: mediaQueryData.size.height,
       decoration: const BoxDecoration(
           gradient: LinearGradient(
-        colors: [Colors.white, Colors.cyan, Colors.white],
-      )),
+            colors: [Colors.white, Colors.cyan, Colors.white],
+          )),
       child: Column(
         children: [
           Padding(
-              padding: EdgeInsets.only(top: mediaQueryData.size.height * 0.1),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  GestureDetector(
-                    onTap:(){
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=>const Additem()));
-                    },
-                    child: Container(
-                      width: 150,
-                      height: 150,
-                      decoration: BoxDecoration(
-                          color: const Color.fromRGBO(116, 149, 255, 0.8),
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: const [
-                            BoxShadow(
-                                color: Colors.black38,
-                                blurRadius: 2,
-                                spreadRadius: 2,
-                                offset: Offset(0, 3))
-                          ]),
-                      child: const Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text("0", style: TextStyle(
-                            fontSize: 30,
+            padding: EdgeInsets.only(top: mediaQueryData.size.height * 0.1),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap:(){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>const Additem()));
+                  },
+                  child: Container(
+                    width: 150,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        color: const Color.fromRGBO(116, 149, 255, 0.8),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: const [
+                          BoxShadow(
+                              color: Colors.black38,
+                              blurRadius: 2,
+                              spreadRadius: 2,
+                              offset: Offset(0, 3))
+                        ]),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text("0", style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.black,
+                          decoration: TextDecoration.none,
+                        ),),
+                        Text(
+                          "Add Item",
+                          style: TextStyle(
+                            fontSize: 20,
                             color: Colors.black,
                             decoration: TextDecoration.none,
-                          ),),
-                          Text(
-                            "Add Item",
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.black,
-                              decoration: TextDecoration.none,
-                            ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
-                  Container(
+                ),
+                GestureDetector(
+                  onTap: (){
+                    EditItem().method();
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>EditItem()));
+                  },
+                  child: Container(
                     width: 150,
                     height: 150,
                     decoration: BoxDecoration(
@@ -77,89 +83,90 @@ class _OwnerState extends State<Owner> {
                               offset: Offset(0, 3))
                         ]),
                     child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children:[
-                      Icon(Icons.edit, color: Colors.black,size: 30,),
-                      Text("Edit Item",style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                      decoration: TextDecoration.none,
-                    ),)]),
-                  )
-                ],
-              ),
+                          Icon(Icons.edit, color: Colors.black,size: 30,),
+                          Text("Edit Item",style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            decoration: TextDecoration.none,
+                          ),)]),
+                  ),
+                )
+              ],
+            ),
           ),
           Padding(
-              padding: EdgeInsets.only(top: mediaQueryData.size.height * 0.1),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    width: 150,
-                    height: 150,
-                    decoration: BoxDecoration(
-                        color: const Color.fromRGBO(137, 244, 224, 0.8),
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: const [
-                          BoxShadow(
-                              color: Colors.black38,
-                              blurRadius: 2,
-                              spreadRadius: 2,
-                              offset: Offset(0, 3))
-                        ]),
-                    child:  const Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text("0",
-                          style: TextStyle(
-                            fontSize: 30,
-                            color: Colors.black,
-                            decoration: TextDecoration.none,
-                          ),),
-                        Text(
-                          "Sale",
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.black,
-                            decoration: TextDecoration.none,
-                          ),
+            padding: EdgeInsets.only(top: mediaQueryData.size.height * 0.1),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                      color: const Color.fromRGBO(137, 244, 224, 0.8),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.black38,
+                            blurRadius: 2,
+                            spreadRadius: 2,
+                            offset: Offset(0, 3))
+                      ]),
+                  child:  const Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text("0",
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.black,
+                          decoration: TextDecoration.none,
+                        ),),
+                      Text(
+                        "Sale",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          decoration: TextDecoration.none,
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                  Container(
-                    width: 150,
-                    height: 150,
-                    decoration: BoxDecoration(
-                        color: const Color.fromRGBO(230, 128, 250, 0.8),
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: const [
-                          BoxShadow(
-                              color: Colors.black38,
-                              blurRadius: 2,
-                              spreadRadius: 2,
-                              offset: Offset(0, 3))
-                        ]),
-                    child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text("0",
-                          style: TextStyle(
-                            fontSize: 30,
-                            color: Colors.black,
-                            decoration: TextDecoration.none,
-                          ),),
-                        Text("User",
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.black,
-                            decoration: TextDecoration.none,
-                          ),),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                ),
+                Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                      color: const Color.fromRGBO(230, 128, 250, 0.8),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.black38,
+                            blurRadius: 2,
+                            spreadRadius: 2,
+                            offset: Offset(0, 3))
+                      ]),
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text("0",
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.black,
+                          decoration: TextDecoration.none,
+                        ),),
+                      Text("User",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          decoration: TextDecoration.none,
+                        ),),
+                    ],
+                  ),
+                )
+              ],
+            ),
           )
         ],
       ),
