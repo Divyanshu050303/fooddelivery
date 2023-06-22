bde1c64f8a6943a348939bb49f889d35569e456e
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-06-22 11:47:23 +0530

    remove unused import

--- lib/Services/SigningClass.dart
+++ lib/Services/SigningClass.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+// import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class SingingAuth extends ChangeNotifier{
@@ -41,9 +41,9 @@ class SingingAuth extends ChangeNotifier{
     FirebaseAuth.instance.signOut();
     _isSigningIn=false;
   }
-  Future FacebookLogin() async{
-    final LoginResult loginResult=await FacebookAuth.instance.login();
-    final OAuthCredential facebookAuthCredential = FacebookAuthProvider.credential(loginResult.accessToken!.token);
-    return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
-  }
+  // Future FacebookLogin() async{
+  //   final LoginResult loginResult=await FacebookAuth.instance.login();
+  //   final OAuthCredential facebookAuthCredential = FacebookAuthProvider.credential(loginResult.accessToken!.token);
+  //   return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+  // }
 }
