773c1716beda8bb05f6ee20dbf1ec6912c8ac4fb
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-07-04 23:18:13 +0530

    Add Address Details

--- lib/screen/addAddress.dart
+++ lib/screen/addAddress.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:fooddelivery/screen/updateAddress.dart';
 import 'Address.dart';
 
 class Address extends StatefulWidget {
@@ -11,6 +15,89 @@ class Address extends StatefulWidget {
 }
 
 class _AddressState extends State<Address> {
+  Query dbref=FirebaseDatabase.instance.ref().child("UserAddress").child(FirebaseAuth.instance.currentUser!.uid);
+  DatabaseReference reference=FirebaseDatabase.instance.ref().child("UserAddress").child(FirebaseAuth.instance.currentUser!.uid);
+Widget listItem({required Map address}){
+  return Container(
+    margin: const EdgeInsets.all(10),
+    padding: const EdgeInsets.all(10),
+    height: 190,
+     decoration:BoxDecoration(
+       gradient: LinearGradient(colors: [Colors.white24,Colors.white24])
+     ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          address['_name'],
+          style: const TextStyle(fontSize: 22, fontWeight: FontWeight.w400,decoration: TextDecoration.none,color: Colors.black),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Text(
+          address['_building'].toString(),
+          style: const TextStyle(fontSize:18 , fontWeight: FontWeight.w400,decoration: TextDecoration.none,color: Colors.black),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Text(
+          address['_road'],
+          style: const TextStyle(fontSize:18 , fontWeight: FontWeight.w400,decoration: TextDecoration.none,color: Colors.black),
+        ),Text(
+          address['_city'],
+          style: const TextStyle(fontSize:18 , fontWeight: FontWeight.w400,decoration: TextDecoration.none,color: Colors.black),
+        ),Text(
+          address['_state']+" "+address['_pincode'],
+          style: const TextStyle(fontSize:18 , fontWeight: FontWeight.w400,decoration: TextDecoration.none,color: Colors.black),
+        ),
+        Text(
+          "Phone Number"+ ":"+address['_phoneNumber'],
+          style: const TextStyle(fontSize:18 , fontWeight: FontWeight.w400,decoration: TextDecoration.none,color: Colors.black),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (_) => UpdateAddress(addressKey: address['key'])));
+              },
+              child: const Row(
+                children: [
+                  Icon(
+                    Icons.edit,
+                    // color: Theme.of(context).primaryColor,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              width: 6,
+            ),
+            GestureDetector(
+              onTap: () {
+                reference.child(address['key']).remove();
+                Fluttertoast.showToast(msg: "Address Deleted Successfully!",toastLength:Toast.LENGTH_SHORT,gravity: ToastGravity.BOTTOM,backgroundColor: Colors.grey,textColor: Colors.white );
+              },
+              child: const Row(
+                children: [
+                  Icon(
+                    Icons.delete,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        )
+      ],
+    ),
+  );
+}
   @override
   Widget build(BuildContext context) {
     MediaQueryData mediaQueryData = MediaQuery.of(context);
@@ -26,14 +113,14 @@ class _AddressState extends State<Address> {
             width: mediaQueryData.size.width,
             height: mediaQueryData.size.height * 0.1,
             decoration: BoxDecoration(
-                gradient: LinearGradient(
+                gradient: const LinearGradient(
                     colors: [Colors.greenAccent, Colors.greenAccent]),
                 boxShadow: [
                   BoxShadow(
                       blurRadius: 2,
                       spreadRadius: 2,
                       color: Colors.black.withOpacity(0.8),
-                      offset: Offset(0, 2))
+                      offset: const Offset(0, 2))
                 ]),
             child: Padding(
               padding: const EdgeInsets.only(top: 14.0),
@@ -94,6 +181,18 @@ class _AddressState extends State<Address> {
                   ],
                 ),
               ),
+            ),
+          ),
+          SizedBox(
+            width: mediaQueryData.size.width,
+            height: mediaQueryData.size.height*0.8,
+            child: FirebaseAnimatedList(
+              query: dbref,
+              itemBuilder: (BuildContext context, DataSnapshot snapshot,Animation<double> animation,int index){
+                Map address=snapshot.value as Map;
+                address['key']=snapshot.key;
+                return listItem(address:address);
+              },
             ),
           )
         ],
