d9735bcf6c438af26d4bee3ace3036f29796d262
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-07-04 23:16:27 +0530

    fetch address form database

---
+++ lib/Services/JsonData.dart
@@ -0,0 +1,12 @@
+
+import 'package:firebase_database/firebase_database.dart';
+
+class AddressWriteRead{
+final databaseref= FirebaseDatabase.instance.ref("UserAddress");
+int total=5;
+void CreateDataBase(String path, Map<String, dynamic> data,String email){
+  databaseref.child(email).child(total.toString()).set(data);
+  total++;
+}
+
+}
