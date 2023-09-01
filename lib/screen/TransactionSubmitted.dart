92a513eca8c4a8a95b27edab0c74ecc1455929d7
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-09-01 15:06:51 +0530

    Transaction Submitted Screen

---
+++ lib/screen/TransactionSubmitted.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TransactionSubmitted extends StatefulWidget {
+  const TransactionSubmitted({super.key});
+
+  @override
+  State<TransactionSubmitted> createState() => _TransactionSubmittedState();
+}
+
+class _TransactionSubmittedState extends State<TransactionSubmitted> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
